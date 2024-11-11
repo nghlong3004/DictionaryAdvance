214f27a8a7898dbb6e6dc7b11b8b07c7a299f774
nghlong3004 <nghlong3004@gmail.com>
2024-11-11 18:42:15 +0700

    Checkout main class

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -4,9 +4,9 @@ import java.awt.Font;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import view.ViewDictionary;
 
 
@@ -16,7 +16,7 @@ public class MainClass {
     FlatRobotoFont.install();
     FlatLaf.registerCustomDefaultsSource("themes");
     UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
-    FlatDarkLaf.setup();
+    FlatMacDarkLaf.setup();
     SwingUtilities.invokeLater(() -> ViewDictionary.install());
   }
 
