6e8a5001706eeac93fcddb051643d397cfe87607
nghlong3004 <nghlong3004@gmail.com>
2024-11-12 22:24:36 +0700

    Fix file

--- src/main/java/util/repository/Google.java
+++ src/main/java/util/repository/Google.java
@@ -1,27 +1,27 @@
 package util.repository;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 
 public class Google {
-  public static final String GOOGLE_AUDIO_API_URL = "http://translate.google.com/translate_tts";
-  public static final String GOOGLE_SEARCH_API_URL = "https://clients1.google.com/complete/search";
-  public static final String GOOGLE_TRANSLATE_API_URL =
-      "https://script.google.com/macros/s/AKfycbwU7msuEL2UyzONn_I9m2HMG9FarUKOlmn6jbKIKhPx8Vd1QkqtxnQokl8R792zRrDQ/exec";
+  private static final String GOOGLE_AUDIO_API_URL = "http://translate.google.com/translate_tts";
+  private static final String GOOGLE_TRANSLATE_URL = "http://translate.google.com/translate_a/single";
 
   public static void speak(String language, String text) throws IOException, URISyntaxException {
     String url = generateSpeakURL(language, text);
@@ -41,23 +41,6 @@ public class Google {
     }
   }
 
-  public static String translate(String text, String langFrom, String langTo) throws IOException {
-    String urlStr = (GOOGLE_TRANSLATE_API_URL) + "?q=" + URLEncoder.encode(text, "UTF-8")
-        + "&source=" + langFrom + "&target=" + langTo;
-    @SuppressWarnings("deprecation")
-    URL url = new URL(urlStr);
-    StringBuilder response = new StringBuilder();
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestProperty("User-Agent", "Mozilla/5.0");
-    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-    String inputLine;
-    while ((inputLine = in.readLine()) != null) {
-      response.append(inputLine);
-    }
-    in.close();
-    return response.toString();
-  }
-
   private static String generateSpeakURL(String language, String text)
       throws UnsupportedEncodingException {
     String url = GOOGLE_AUDIO_API_URL + "?ie=UTF-8" + "&q=" + URLEncoder.encode(text, "UTF-8")
@@ -65,24 +48,143 @@ public class Google {
     return url;
   }
 
-  private static int[] TKK() {
-    return new int[] {0x6337E, 0x217A58DC + 0x5AF91132};
+  private static String generateURL(String sourceLanguage, String targetLanguage, String text)
+      throws UnsupportedEncodingException {
+    String encoded = URLEncoder.encode(text, "UTF-8"); // Encode
+    StringBuilder sb = new StringBuilder();
+    sb.append(GOOGLE_TRANSLATE_URL);
+    sb.append("?client=webapp"); // The client parameter
+    sb.append("&hl=en"); // The language of the UI?
+    sb.append("&sl="); // Source language
+    sb.append(sourceLanguage);
+    sb.append("&tl="); // Target language
+    sb.append(targetLanguage);
+    sb.append("&q=");
+    sb.append(encoded);
+    sb.append("&multires=1");// Necessary but unknown parameters
+    sb.append("&otf=0");
+    sb.append("&pc=0");
+    sb.append("&trs=1");
+    sb.append("&ssel=0");
+    sb.append("&tsel=0");
+    sb.append("&kc=1");
+    sb.append("&dt=t");// This parameters requests the translated text back.
+    // Other parameters request additional information such as pronunciation, and so on.
+    // TODO Modify API so that the user may request this additional information.
+    sb.append("&ie=UTF-8"); // Input encoding
+    sb.append("&oe=UTF-8"); // Output encoding
+    sb.append("&tk="); // Token authentication parameter
+    sb.append(generateToken(text));
+    return sb.toString();
   }
 
-  private static long shr64(long x, int bits) {
+  public static String detectLanguage(String text) throws IOException {
+    String urlText = generateURL("auto", "en", text);
+    URI uri = null;
+    try {
+      uri = new URI(urlText);
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
+    URL obj = uri.toURL();
+    String rawData = urlToText(obj);// Gets text from Google
+    return findLanguage(rawData);
+  }
+
+  public static String translate(String text) throws IOException {
+    return translate(Locale.getDefault().getLanguage(), text);
+  }
+
+  public static String translate(String text, String targetLanguage) throws IOException {
+    return translate("auto", targetLanguage, text);
+  }
+
+  public static String translate(String text, String sourceLanguage, String targetLanguage)
+      throws IOException {
+    String urlText = generateURL(sourceLanguage, targetLanguage, text);
+    URI uri = null;
+    try {
+      uri = new URI(urlText);
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
+    URL obj = uri.toURL();
+    String rawData = urlToText(obj);// Gets text from Google
+    if (rawData == null) {
+      return null;
+    }
+    String[] raw = rawData.split("\"");// Parses the JSON
+    if (raw.length < 2) {
+      return null;
+    }
+    return raw[1];// Returns the translation
+  }
+
+  private static String urlToText(URL url) throws IOException {
+    URLConnection urlConn = url.openConnection(); // Open connection
+    // Adding header for user agent is required. Otherwise, Google rejects the request
+    urlConn.addRequestProperty("User-Agent",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:2.0) Gecko/20100101 Firefox/4.0");
+    Reader r = new java.io.InputStreamReader(urlConn.getInputStream(), Charset.forName("UTF-8"));
+    StringBuilder buf = new StringBuilder();
+    while (true) {// Reads String from buffer
+      int ch = r.read();
+      if (ch < 0)
+        break;
+      buf.append((char) ch);
+    }
+    String str = buf.toString();
+    return str;
+  }
+
+  private static String findLanguage(String rawData) {
+    for (int i = 0; i + 5 < rawData.length(); i++) {
+      boolean dashDetected = rawData.charAt(i + 4) == '-';
+      if (rawData.charAt(i) == ',' && rawData.charAt(i + 1) == '"'
+          && ((rawData.charAt(i + 4) == '"' && rawData.charAt(i + 5) == ',') || dashDetected)) {
+        if (dashDetected) {
+          int lastQuote = rawData.substring(i + 2).indexOf('"');
+          if (lastQuote > 0)
+            return rawData.substring(i + 2, i + 2 + lastQuote);
+        } else {
+          String possible = rawData.substring(i + 2, i + 4);
+          if (containsLettersOnly(possible)) {// Required due to Google's inconsistent formatting.
+            return possible;
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  private static boolean containsLettersOnly(String text) {
+    for (int i = 0; i < text.length(); i++) {
+      if (!Character.isLetter(text.charAt(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static int[] TKK() {
+    int[] tkk = {0x6337E, 0x217A58DC + 0x5AF91132};
+    return tkk;
+  }
+
+  private static int shr32(int x, int bits) {
     if (x < 0) {
-      long x_l = 0xffffffffffffffffL + x + 1;
-      return x_l >> bits;
+      long x_l = 0xffffffffl + x + 1;
+      return (int) (x_l >> bits);
     }
     return x >> bits;
   }
 
-  private static long RL(long a, String b) {
+  private static int RL(int a, String b) {// I am not entirely sure what this magic does.
     for (int c = 0; c < b.length() - 2; c += 3) {
       int d = b.charAt(c + 2);
-      d = (d >= 'A') ? d - 87 : d - '0';
-      d = (int) (b.charAt(c + 1) == '+' ? shr64(a, d) : (a << d));
-      a = b.charAt(c) == '+' ? (a + (d & 0xffffffffffffffffL)) : a ^ d;
+      d = d >= 65 ? d - 87 : d - 48;
+      d = b.charAt(c + 1) == '+' ? shr32(a, d) : (a << d);
+      a = b.charAt(c) == '+' ? (a + (d & 0xFFFFFFFF)) : a ^ d;
     }
     return a;
   }
@@ -92,7 +194,7 @@ public class Google {
     int b = tkk[0];
     int e = 0;
     int f = 0;
-    ArrayList<Integer> d = new ArrayList<Integer>();
+    List<Integer> d = new ArrayList<Integer>();
     for (; f < text.length(); f++) {
       int g = text.charAt(f);
       if (0x80 > g) {
@@ -115,7 +217,7 @@ public class Google {
       }
     }
 
-    long a_i = b;
+    int a_i = b;
     for (e = 0; e < d.size(); e++) {
       a_i += d.get(e);
       a_i = RL(a_i, "+-a^+6");
